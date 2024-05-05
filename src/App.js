7692b6fccf4e95c10362ff8257d51b9e2d755690
manan2501 <kevadiyamanan@gmail.com>
2024-05-05 14:56:50 +0530

    prettier

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { fetchJobsData } from "./features/jobSearchSlice";
 import { useEffect } from "react";
 
 function App() {
-        const { jobs, totalJobs, loading, error } = useSelector(
-                (state) => state.jobSearch
-        );
-        const dispatch = useDispatch();
+    const { jobs, totalJobs, loading, error } = useSelector(
+        state => state.jobSearch
+    );
+    const dispatch = useDispatch();
 
-        useEffect(() => {
-                dispatch(fetchJobsData({ limit: 10, offset: 0 }));
-        }, []);
+    useEffect(() => {
+        dispatch(fetchJobsData({ limit: 10, offset: 0 }));
+    }, []);
 
-        console.log("jobSearch: ", { jobs, totalJobs, loading, error });
+    console.log("jobSearch: ", { jobs, totalJobs, loading, error });
 
-        return <div>Clean React App</div>;
+    return <div>Clean React App</div>;
 }
 
 export default App;
--- src/features/jobSearchSlice.js
+++ src/features/jobSearchSlice.js
@@ -1,55 +1,52 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchJobsData = createAsyncThunk(
-        "jobSearch/fetchData",
-        async ({ limit, offset }, thunkAPI) => {
-                try {
-                        const response = await fetch(
-                                "https://api.weekday.technology/adhoc/getSampleJdJSON",
-                                {
-                                        method: "POST",
-                                        headers: {
-                                                "Content-Type":
-                                                        "application/json",
-                                        },
-                                        body: JSON.stringify({ limit, offset }),
-                                }
-                        );
-                        const data = await response.json();
-                        return data;
-                } catch (error) {
-                        return thunkAPI.rejectWithValue(error.message);
+    "jobSearch/fetchData",
+    async ({ limit, offset }, thunkAPI) => {
+        try {
+            const response = await fetch(
+                "https://api.weekday.technology/adhoc/getSampleJdJSON",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ limit, offset })
                 }
+            );
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
         }
+    }
 );
 
 export const jobSearchSlice = createSlice({
-        name: "jobSearch",
-        initialState: {
-                jobs: [],
-                totalJobs: 0,
-                loading: false,
-                error: null,
-        },
-        reducers: {},
-        extraReducers: (builder) => {
-                builder.addCase(fetchJobsData.pending, (state) => {
-                        state.loading = true;
-                        state.error = null;
-                })
-                        .addCase(fetchJobsData.fulfilled, (state, action) => {
-                                state.loading = false;
-                                state.totalJobs = action.payload.totalCount;
-                                state.jobs = [
-                                        ...state.jobs,
-                                        ...action.payload.jdList,
-                                ];
-                        })
-                        .addCase(fetchJobsData.rejected, (state, action) => {
-                                state.loading = false;
-                                state.error = action.payload;
-                        });
-        },
+    name: "jobSearch",
+    initialState: {
+        jobs: [],
+        totalJobs: 0,
+        loading: false,
+        error: null
+    },
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(fetchJobsData.pending, state => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchJobsData.fulfilled, (state, action) => {
+                state.loading = false;
+                state.totalJobs = action.payload.totalCount;
+                state.jobs = [...state.jobs, ...action.payload.jdList];
+            })
+            .addCase(fetchJobsData.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            });
+    }
 });
 
 export default jobSearchSlice.reducer;
