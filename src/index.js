73935fa73c2b0915331ec5a1b9e8989772490896
manan2501 <kevadiyamanan@gmail.com>
2024-05-05 14:21:17 +0530

    redux store setup

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-        <React.StrictMode>
+        <Provider store={store}>
                 <App />
-        </React.StrictMode>
+        </Provider>
 );
